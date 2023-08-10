f655800698eb33f9ecb7e65401011b76d5e10022
Adunniagboola <adeolaagboola84@gmail.com>
2023-08-10 08:28:00 -0700

    data size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * main - print various size of data type
+ *
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	printf("Size of a char: ");
+	printf("Size of an int: ");
+	printf("Size of a long int: ");
+	printf("Size of a long long int: ");
+	printf("Size of a float: ");
+	return (0);
+}
