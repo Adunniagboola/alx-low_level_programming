00081e0812b7a329a80f96ea963f33262a0f6494
Adunniagboola <adeolaagboola84@gmail.com>
2023-08-15 14:08:35 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Function that computes the absolute value of an integrer
+ * @i: the int to check
+ * Return: the absolute value of int
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	return (-i);
+}
