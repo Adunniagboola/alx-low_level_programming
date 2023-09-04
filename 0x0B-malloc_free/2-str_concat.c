2be639ecb165b632ea7b4afe03fe78d1c667ccef
Adunniagboola <adeolaagboola84@gmail.com>
2023-09-04 10:37:35 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - function that concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: result
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *conct;
+	int k, ci;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	k = ci = 0;
+	while (s1[k] != '\0')
+		k++;
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (k + ci + 1));
+
+	if (conct == NULL)
+		return (NULL);
+	k = ci = 0;
+	while (s1[k] != '\0')
+	{
+		conct[k] = s1[k];
+		k++;
+	}
+
+	while (s2[ci] != '\0')
+	{
+		conct[k] = s2[ci];
+		k++, ci++;
+	}
+	conct[k] = '\0';
+	return (conct);
+}
